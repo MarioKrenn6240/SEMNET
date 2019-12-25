8fbd0fa9cb34475f8089ffb754cbe93ecb1c6496
Mario Krenn <46604014+MarioKrenn6240@users.noreply.github.com>
2019-12-25 01:25:52 -0500

    Add files via upload

---
+++ CreateAndAnalyseSEMNET/GetValues.m
@@ -0,0 +1,10 @@
+c=clock;
+timenow=[num2str(c(1)) num2str(c(2)) num2str(c(3))];
+
+% produced a NN which was trained to predict yearlimits(ii) with data from yearlimits(ii)-PredictionInterval
+OverallNum=0;
+
+PredictionInterval=3;
+yearlimits=fliplr([2004:2017]);
+
+
