41b8b933e190eefd4e1b2392624385df89535027
Mario Krenn <46604014+MarioKrenn6240@users.noreply.github.com>
2020-01-14 05:51:14 -0500

    Add files via upload

--- python/create_SemNet.py
+++ python/create_SemNet.py
@@ -1,3 +1,22 @@
+#########
+# Predicting Research Trends with Semantic and Neural Networks with an application in Quantum Physics
+# Mario Krenn, Anton Zeilinger
+# https://arxiv.org/abs/1906.06843 (in PNAS)
+#
+# This file is the navigator for creating a semantic network from data,
+# and using it for predicting future research trends using artificial
+# neural networks.
+# 
+# The code is far from clean, but should give the principle idea.
+# If you have any questions, dont hasitate to contact me:
+# mario.krenn@univie.ac.at / mariokrenn.wordpress.com
+#
+#
+#
+# Mario Krenn, Toronto, Canada, 14.01.2020
+#
+#########
+
 import sys
 
 sys.path.append('1aCreateFullArticleData')
@@ -6,6 +25,7 @@ sys.path.append('2CreateNetwork')
 sys.path.append('3CollapsNetwork')
 sys.path.append('4CalculateAncientNetworks')
 sys.path.append('5PrepareNNData')
+sys.path.append('6TrainNN')
 
 from create_full_article_data import create_full_data_arxiv
 from create_full_article_data_APS import create_full_data_APS
@@ -14,6 +34,7 @@ from analyse_network import collaps_network
 from prepare_ancient_semnets import create_ancient_networks
 from calc_properties import calculate_all_network_properties
 from prepare_training_data import prepare_training_data
+from train_nn import train_nn
 
 full_data_arxiv=create_full_data_arxiv()
 full_data_APS=create_full_data_APS()
@@ -23,8 +44,8 @@ network_T_full,nn_full,all_KW_full=create_network(all_papers)
 
 network_T,nn,all_KW=collaps_network(network_T_full,nn_full,all_KW_full)
 
-start_year=2000
-end_year=2017
+start_year=2005
+end_year=2018
 all_KW,evolving_nets,evolving_nums=create_ancient_networks(network_T,nn,all_KW,start_year,end_year)
 
 all_properties=calculate_all_network_properties(evolving_nets,evolving_nums)
@@ -32,6 +53,5 @@ all_properties=calculate_all_network_properties(evolving_nets,evolving_nums)
 prediction_distance=5 # how many years into the future are we predicting
 all_data_0, all_data_1=prepare_training_data(evolving_nets,all_properties,prediction_distance,start_year)
 
-# 6TrainNN) Train network, we will use PyTorch
-
-# 7bCalcROC) Evaluate the Quality of the predictions using ROC and AUC
+train_nn(all_data_0, all_data_1, prediction_distance, start_year) # Here we train the neural network, and calculate the ROC & AUC
+# ROC calculation is on TODO list
