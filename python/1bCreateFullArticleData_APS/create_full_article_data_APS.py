032ee5c9d784dd286920c554d5266dd3a6dc7dbf
Mario Krenn <46604014+MarioKrenn6240@users.noreply.github.com>
2020-01-17 01:20:40 -0500

    Update create_full_article_data_APS.py

--- python/1bCreateFullArticleData_APS/create_full_article_data_APS.py
+++ python/1bCreateFullArticleData_APS/create_full_article_data_APS.py
@@ -2,6 +2,7 @@ import os, json
 from pathlib import Path
 
 def create_full_data_APS():
+    # Reads publication data from APS json files
     full_article_data_APS=[]
     aps_dir = Path('1bCreateFullArticleData_APS/APS-data')
     for newdir in aps_dir.iterdir():
